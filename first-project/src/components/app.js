09463732d730c33aeefc355e489bd9413145ffd7
SeonHyungJo <seonhyung.jo@gmail.com>
2020-12-13 15:51:55 +0900

    Create tutorial project

---
+++ first-project/src/components/app.js
@@ -0,0 +1,20 @@
+import { Router } from 'preact-router';
+
+import Header from './header';
+
+// Code-splitting is automated for `routes` directory
+import Home from '../routes/home';
+import Profile from '../routes/profile';
+
+const App = () => (
+	<div id="app">
+		<Header />
+		<Router>
+			<Home path="/" />
+			<Profile path="/profile/" user="me" />
+			<Profile path="/profile/:user" />
+		</Router>
+	</div>
+)
+
+export default App;
