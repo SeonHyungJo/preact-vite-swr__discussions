2733adda0eaaa0c9caeac6c2982cf3b3c78b5f41
SeonHyungJo <seonhyung.jo@gmail.com>
2020-12-13 18:56:48 +0900

    Add PWA Plugin in preact-vite-ts project

--- preact-vite-ts-project/vite.config.ts
+++ preact-vite-ts-project/vite.config.ts
@@ -1,4 +1,5 @@
 import preactRefresh from '@prefresh/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import type { UserConfig } from 'vite'
 
 const config: UserConfig = {
@@ -6,7 +7,32 @@ const config: UserConfig = {
     factory: 'h',
     fragment: 'Fragment'
   },
-  plugins: [preactRefresh()]
+  plugins: [
+    VitePWA({
+      manifest: {
+        "name": "first-project",
+        "short_name": "first-project",
+        "start_url": "/",
+        "display": "standalone",
+        "orientation": "portrait",
+        "background_color": "#fff",
+        "theme_color": "#673ab8",
+        "icons": [
+          {
+            "src": "/assets/icons/android-chrome-192x192.png",
+            "type": "image/png",
+            "sizes": "192x192"
+          },
+          {
+            "src": "/assets/icons/android-chrome-512x512.png",
+            "type": "image/png",
+            "sizes": "512x512"
+          }
+        ]
+      }
+    }),
+    preactRefresh()
+  ]
 }
 
 export default config
